dec2bd42579d69b7dcbf7903fa341e4eed939215
JackronyK <karani.riungu.a@gmail.com>
2024-03-07 14:23:51 +0300

    Updating

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 
-class DataCleaner:
+class InvoiceCleaner:
     def __init__(self, df):
         self.df = df
 
@@ -11,7 +12,7 @@ class DataCleaner:
         if "No." in self.df.columns:
             self.df.drop(self.df[self.df["No."] == "Total"].index, inplace=True)
 
-    def pick_invoice_no_column_name(self):
+    def pick_invoice_column_name(self):
         for substring in ['invoice no', 'ref']:
             matching_columns = [col for col in self.df.columns if substring.lower() in col.lower()]
             if matching_columns:
@@ -30,7 +31,7 @@ class DataCleaner:
                         max_col = field
         return max_col
 
-    def col_of_interest(self):
+    def clean_data(self):
         self.drop_null_rows()
         self.drop_total_row()
 
@@ -38,29 +39,22 @@ class DataCleaner:
             "Date": "Invoice Date",
             "ID": "Link ID",
             "Period": "Invoice Period",
-            "Description": "Invoice Description",            
-            "Reference/No": self.pick_invoice_no_column_name(),
+            "Description": "Invoice Description",
+            "Reference/No": self.pick_invoice_column_name(),
             "QRC": self.QRC_Finder()
         }
 
         self.df = self.df.loc[:, [value for value in columns_wanted.values()]]
 
-
-    def col_renaming(self):
-        self.col_of_interest()
-
-        new_col_names = {
+        new_invoice_col_names = {
             "Invoice Date": "Invoice_Data",
             "Link ID": "Link_ID",
             "Invoice Period": "Invoice_Period",
-            "Invoice Description":"Invoice_Description",           
-            self.pick_invoice_no_column_name(): "Invoice_Reference",
-             self.QRC_Finder(): "Total_QRC"
+            "Invoice Description": "Invoice_Description",
+            self.pick_invoice_column_name(): "Invoice_Reference",
+            self.QRC_Finder(): "Total_QRC"
         }
-        self.df = self.df.rename(columns = new_col_names)
-
-    def clean_invoice_data(self):
-        self.col_renaming()
+        self.df = self.df.rename(columns=new_invoice_col_names)
 
         # Adjust 'Link ID' based on its current type
         if 'Link_ID' in self.df.columns:
@@ -68,13 +62,98 @@ class DataCleaner:
                 self.df['Link_ID'] = self.df['Link_ID'].astype('int').astype('str')
             elif self.df['Link_ID'].dtype == 'int':
                 self.df['Link_ID'] = self.df['Link_ID'].astype('str')
-            # If 'Link ID' is already object, no conversion is needed
-            # Convert 'Invoice Date' to datetime
-        if 'Invoice_Date' in self.df.columns:
-            self.df['Invoice_Date'] = pd.to_datetime(self.df['Invoice_Date'])
 
-        
-    def invoice_df(self):
-        self.clean_invoice_data()
-        df = self.df
-        return df
+        # Convert 'Invoice Date' to datetime
+        if 'Invoice_Data' in self.df.columns:
+            self.df['Invoice_Data'] = pd.to_datetime(self.df['Invoice_Data'])
+
+        return self.df
+
+
+class SLACleaner:
+    def __init__(self, df):
+        self.df = df
+
+    def drop_null_rows(self):
+        self.df.dropna(how='all', inplace=True)
+
+    def drop_total_row(self):
+        if "No." in self.df.columns:
+            self.df.drop(self.df[self.df["No."] == "Total"].index, inplace=True)
+
+    def capacity_picker(self):
+        for pat in ['Capacity']:
+            matching_cols = [col for col in self.df.columns if pat.lower() in col.lower()]
+            if matching_cols:
+                return matching_cols[0]
+
+    def comments_picker(self):
+        for pat in ['comment']:
+            matching_cols = [col for col in self.df.columns if pat.lower() in col.lower()]
+            if matching_cols:
+                return matching_cols[0]
+
+    def site_desc_picker(self):
+        for pat in ['site', 'description']:
+            matching_cols = [col for col in self.df.columns if pat.lower() in col.lower()]
+            if matching_cols:
+                return matching_cols[0]
+
+    def MRC_picker(self):
+        for pat in ['month', 'MRC']:
+            matching_cols = [col for col in self.df.columns if pat.lower() in col.lower()]
+            if matching_cols:
+                return matching_cols[0]
+
+    def clean_data(self):
+        self.drop_null_rows()
+        self.drop_total_row()
+
+        SLA_Cols = {
+            'Link ID': 'Link ID',
+            'SLA Dates': 'SLA Date',
+            'Last Mile': 'Last Mile',
+            'Capacity': self.capacity_picker(),
+            'Sites/Location': self.site_desc_picker(),
+            'MRC': self.MRC_picker(),
+            'Comments': self.comments_picker()
+        }
+
+        self.df = self.df.loc[:, [col for col in SLA_Cols.values()]]
+
+        # Changing the link id to str depending on the current dtype
+        if 'Link ID' in self.df.columns:
+            if self.df['Link ID'].dtype == 'float':
+                self.df['Link ID'] = self.df['Link ID'].astype('int').astype('str')
+            elif self.df['Link ID'].dtype == 'int':
+                self.df['Link ID'] = self.df['Link ID'].astype('str')
+
+        # Convert 'SLA Date' to datetime
+        if 'SLA Date' in self.df.columns:
+            self.df['SLA Date'] = pd.to_datetime(self.df['SLA Date'])
+
+        # Capacity
+        if self.capacity_picker() in self.df.columns and self.df[self.capacity_picker()].dtype == 'object':
+            self.df[self.capacity_picker()] = self.df[self.capacity_picker()].str.split(' ', expand=True)[0].astype(
+                'int')
+
+        # Calculating the Quarterly Recurring Charges
+        if self.MRC_picker() in self.df.columns:
+            self.excise_duty = 0.15
+            self.VAT = 0.16
+            self.df['QRC'] = np.multiply(np.multiply(self.df[self.MRC_picker()], (1 + self.excise_duty + self.VAT)), 3)
+
+        # Renaming the Columns
+        SLA_New_Names = {
+            'Link ID': 'Link_ID',
+            'SLA Date': 'SLA_Date',
+            'Last Mile': 'Last_Mile',
+            'QRC': 'QRC_Incl',
+            self.site_desc_picker(): 'Location',
+            self.capacity_picker(): 'Capacity',
+            self.MRC_picker(): 'MRC_Excl',
+            self.comments_picker(): 'SLM_Comments'
+        }
+        self.df = self.df.rename(columns=SLA_New_Names)
+
+        return self.df
